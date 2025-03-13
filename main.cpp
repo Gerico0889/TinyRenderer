c4ef822e4a4429983c9ce5db0c720a8053e82eaa
Luigi Zaccone <luigi.zaccone@egym.com>
2025-03-13 16:42:27 +0100

    Somehow drawing a wireframe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include "model.h"
 #include "tgaimage.h"
 
 #include <cmath>
+#include <iostream>
 
 constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green = {0, 255, 0, 255};
@@ -20,7 +22,7 @@ void drawLine(int ax, int ay, int bx, int by, TGAImage& framebuffer, const TGACo
         std::swap(ay, by);
     }
 
-    for (float x = ax; x <= bx; ++x) {
+    for (int x = ax; x <= bx; ++x) {
         float t = (x - ax) / static_cast<float>(bx - ax);
         int y = std::round(ay + (by - ay) * t);
         if (steep) {
@@ -31,18 +33,39 @@ void drawLine(int ax, int ay, int bx, int by, TGAImage& framebuffer, const TGACo
     }
 }
 
+void drawLine3d(const Vertex& vertex1, const Vertex& vertex2, TGAImage& framebuffer, const TGAColor& color) {
+    float normalized_x1 = (vertex1.getX() + 1) * 0.5 * framebuffer.width();
+    float normalized_y1 = (vertex1.getY() + 1) * 0.5 * framebuffer.height();
+    float normalized_x2 = (vertex2.getX() + 1) * 0.5 * framebuffer.width();
+    float normalized_y2 = (vertex2.getY() + 1) * 0.5 * framebuffer.height();
+    for (float t = 0; t <= 1; t += 0.01) {
+        int x = normalized_x1 + t * (normalized_x2 - normalized_x1);
+        int y = normalized_y1 + t * (normalized_y2 - normalized_y1);
+
+        framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
-    constexpr int width = 64;
-    constexpr int height = 64;
+    constexpr int width = 2000;
+    constexpr int height = 2000;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax = 7, ay = 3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
+    Model diablo3;
+    diablo3.loadVerticesFromObj("diablo3_pose/diablo3_pose.obj");
+    diablo3.loadFacesFromObjs("diablo3_pose/diablo3_pose.obj");
 
-    drawLine(ax, ay, bx, by, framebuffer, red);
-    drawLine(ax, ay, cx, cy, framebuffer, green);
-    drawLine(cx, cy, bx, by, framebuffer, blue);
+    auto const& vertices = diablo3.getVertices();
+    auto const& faces = diablo3.getFaces();
+    for (auto const& indices : faces) {
+        auto const index1 = std::get<0>(indices);
+        auto const index2 = std::get<1>(indices);
+        auto const index3 = std::get<2>(indices);
+
+        drawLine3d(vertices[index1], vertices[index2], framebuffer, red);
+        drawLine3d(vertices[index2], vertices[index3], framebuffer, red);
+        drawLine3d(vertices[index3], vertices[index1], framebuffer, red);
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
